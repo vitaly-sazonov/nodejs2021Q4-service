f02ed15a8accf3d390f37fb8b503b3f12ab1f285
vitomax <v.o.sazonov@gmail.com>
2021-12-05 20:55:16 +0300

    feat: add routes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ const AutoLoad = require('fastify-autoload');
 
 module.exports = async (fastify, opts) => {
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'resources'),
+    dir: path.join(__dirname, 'resources/users'),
+    maxDepth: 3,
+    indexPattern: /.*\.routes(\.ts|\.js|\.cjs|\.mjs)$/,
+    options: {
+      ...opts,
+    },
+  });
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'resources/boards'),
+    maxDepth: 3,
+    indexPattern: /.*\.routes(\.ts|\.js|\.cjs|\.mjs)$/,
+    options: {
+      ...opts,
+    },
+  });
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'resources/tasks'),
     maxDepth: 3,
     indexPattern: /.*\.routes(\.ts|\.js|\.cjs|\.mjs)$/,
     options: {
