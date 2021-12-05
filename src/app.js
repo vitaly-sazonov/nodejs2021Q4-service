a33f385fe8daf6f39617c021776bbd0aef660242
vitomax <v.o.sazonov@gmail.com>
2021-12-05 14:40:16 +0300

    feat: add autoload for routes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,15 @@
-const express = require('express');
-const swaggerUI = require('swagger-ui-express');
+'use strict';
+
 const path = require('path');
-const YAML = require('yamljs');
-const userRouter = require('./resources/users/user.router');
+const AutoLoad = require('fastify-autoload');
 
-const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
-app.use(express.json());
-
-app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
-app.use('/', (req, res, next) => {
-  if (req.originalUrl === '/') {
-    res.send('Service is running!');
-    return;
-  }
-  next();
-});
-
-app.use('/users', userRouter);
-
-module.exports = app;
+module.exports = async (fastify, opts) => {
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'resources'),
+    maxDepth: 3,
+    indexPattern: /.*\.routes(\.ts|\.js|\.cjs|\.mjs)$/,
+    options: {
+      ...opts,
+    },
+  });
+};
