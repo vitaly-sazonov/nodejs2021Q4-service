93036a5207d9bdee9e6b51454b3600741d761d44
vitomax <v.o.sazonov@gmail.com>
2021-12-08 20:33:13 +0300

    refactor: to typescript

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,11 @@
-module.exports = {
-  users: new Map(),
-  boards: new Map(),
-  columns: new Map(),
-  tasks: new Map(),
+import UserType from './resources/users/user.model';
+import BoardType from './resources/boards/board.model';
+import TaskType from './resources/tasks/tasks.model';
+
+const db = {
+  users: new Map<UUIDType, UserType>(),
+  boards: new Map<UUIDType, BoardType>(),
+  tasks: new Map<UUIDType, TaskType>(),
 };
+
+export default db;
