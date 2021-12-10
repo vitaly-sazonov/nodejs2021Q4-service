c0a7ce0b5187f9baf3c5ad6b4728442f5024ec1c
vitomax <v.o.sazonov@gmail.com>
2021-12-10 22:26:27 +0300

    fix: delete unnecessary files

--- src/common/wrapError.ts
+++
@@ -1,9 +0,0 @@
-module.exports = (func) => async (req, reply) => {
-  try {
-    const result = await func(req, reply);
-    return result;
-  } catch (e) {
-    reply.code(404);
-    return e.message;
-  }
-};
