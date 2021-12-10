11ce3c198934c7d8fdc9fa8c4225dc5fdc8a6f3f
vitomax <v.o.sazonov@gmail.com>
2021-12-11 00:50:49 +0300

    feat: add typing column

--- src/resources/columns/column.model.ts
+++ src/resources/columns/column.model.ts
@@ -1,14 +1,36 @@
+export type ColumnType = { id: string; title: string; order: number };
+
+/**
+ * Class Column format.
+ */
 class Column {
-  constructor({ id, title, order }) {
+  /** @public uuid record */
+  id: string;
+  /** @public title column */
+  title: string;
+  /** @public the order of the columns in the board */
+  order: number;
+
+  /**
+   * Constructor class Column
+   * @param object - data column format \{id, title, order\}
+   * @returns Instance class Column
+   */
+  constructor({ id, title, order }: ColumnType) {
     this.id = id;
     this.title = title;
     this.order = order;
   }
 
-  static toResponse(task) {
-    const { id, title, order } = task;
+  /**
+   * Static formating function
+   * @param column - instance class Column
+   * @returns object - truncated data format to \{id, title, order\}
+   */
+  static toResponse(column: ColumnType) {
+    const { id, title, order } = column;
     return { id, title, order };
   }
 }
 
-module.exports = Column;
+export default Column;
