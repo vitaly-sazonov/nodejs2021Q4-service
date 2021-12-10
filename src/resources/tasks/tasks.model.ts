e0f73dbcd5f203f677183ddd672505378ec88c3b
vitomax <v.o.sazonov@gmail.com>
2021-12-11 00:47:21 +0300

    feat: add typing block tasks

--- src/resources/tasks/tasks.model.ts
+++ src/resources/tasks/tasks.model.ts
@@ -1,5 +1,27 @@
+export type TaskType = { id: string; title: string; order: number; description: string; userId: string | null; boardId: string; columnId: string };
+
 class Task {
-  constructor({ id, title, order, description, userId, boardId, columnId }) {
+  /** @public record uuid */
+  id: string;
+  /** @public title column */
+  title: string;
+  /** @public the order of the task in the list */
+  order: number;
+  /** @public task description */
+  description: string;
+  /** @public user uuid */
+  userId: string | null;
+  /** @public board uuid */
+  boardId: string;
+  /** @public board uuid */
+  columnId: string;
+
+  /**
+   * Constructor class Task
+   * @param object - data task format \{ id, title, order, description, userId, boardId, columnId \}
+   * @returns Instance class Task
+   */
+  constructor({ id, title, order, description, userId, boardId, columnId }: TaskType) {
     this.id = id;
     this.title = title;
     this.order = order;
@@ -9,10 +31,15 @@ class Task {
     this.columnId = columnId;
   }
 
-  static toResponse(task) {
+  /**
+   * Static formating function
+   * @param task - instance class Task
+   * @returns object - truncated data format to \{id, title, order, description, userId, boardId, columnId\}
+   */
+  static toResponse(task: TaskType) {
     const { id, title, order, description, userId, boardId, columnId } = task;
     return { id, title, order, description, userId, boardId, columnId };
   }
 }
 
-module.exports = Task;
+export default Task;
--- src/resources/tasks/tasks.repository.ts
+++ src/resources/tasks/tasks.repository.ts
@@ -1,14 +1,30 @@
-const uuid = require('uuid');
+import { randomUUID } from 'crypto';
+import Task, { TaskType } from './tasks.model';
+import { ResourceError } from '../../common/errors';
 
-const Task = require('./tasks.model');
-const db = require('../../db');
+import db from '../../db';
 
+/**
+ * Class TaskRepository for accessing Task data
+ */
 class TaskRepository {
+  /** @internal _tasks - reference to simulate base */
+  _tasks;
+
+  /**
+   * Constructor class TaskRepository
+   * @returns Instance class TaskRepository
+   */
   constructor() {
     this._tasks = db.tasks;
   }
 
-  getAll(boardId) {
+  /**
+   * Get all tasks or all tasks of board from database
+   * @param boardId - board uuid
+   * @returns array of objects data the task
+   */
+  getAll(boardId?: UUIDType): Task[] {
     let tasks;
     tasks = Array.from(this._tasks.values());
     if (boardId) {
@@ -17,49 +33,83 @@ class TaskRepository {
     return tasks;
   }
 
-  add(boardId, task) {
-    const id = uuid.v4();
+  /**
+   * Add task in db
+   * @param boardId - board uuid
+   * @param task - data task \{title, order, description, userId, boardId, columnId\}
+   * @returns object - data format to \{id, title, order, description, userId, boardId, columnId\}
+   */
+  add(boardId: UUIDType, task: TaskType): TaskType {
+    const id = randomUUID();
     const instance = new Task({ ...task, id, boardId });
     this._tasks.set(instance.id, instance);
-    return Task.toResponse(instance);
+    return Task.toResponse(instance as TaskType);
   }
 
-  get(boardId, id) {
+  /**
+   * Get task record from db
+   * @param boardId - board uuid
+   * @param id - task uuid
+   * @returns instance Task
+   */
+  get(boardId: UUIDType, id: UUIDType): Task {
     const task = this._tasks.get(id);
-
     if (task && task.boardId === boardId) {
       return task;
     }
-
-    throw new Error('Task was not founded!');
+    throw new ResourceError('task', 404, 'Task was not founded!');
   }
 
-  update(boardId, id, body) {
+  /**
+   * Update task record from db
+   * @param boardId - board uuid
+   * @param id - task uuid
+   * @param body - new task data of record
+   * @returns object task \{id, title, order, description, userId, boardId, columnId\}
+   */
+  update(boardId: UUIDType, id: UUIDType, body: TaskType): TaskType {
     if (!this._tasks.has(id)) {
-      throw new Error('Task was not founded!');
+      throw new ResourceError('task', 404, 'Task was not founded!');
     }
-    return this._tasks.set(id, body);
+    this._tasks.set(id, body);
+    return body;
   }
 
-  remove(boardId, id) {
+  /**
+   * Delete task record from db
+   * @param boardId - board uuid
+   * @param id - task uuid
+   * @returns returns boolean type of query result
+   */
+  remove(boardId: UUIDType, id: UUIDType): boolean {
     return this._tasks.delete(id);
   }
 
-  removeMany(boardId) {
+  /**
+   * Delete all task related with board from db
+   * @param boardId - board uuid
+   * @returns void
+   */
+  removeMany(boardId: UUIDType): void {
     const tasks = this._tasks;
     this.getAll(boardId).map(({ id }) => tasks.delete(id));
   }
 
-  userNulling(userId) {
+  /**
+   * Find all task the user and nulling userId field in db
+   * @param userId - user uuid
+   * @returns void
+   */
+  userNulling(userId: UUIDType): void {
     const tasksMap = this._tasks;
     this.getAll()
       .filter((task) => task.userId === userId)
       .map(({ id }) => {
-        const task = tasksMap.get(id);
+        const task = tasksMap.get(id) as TaskType;
         task.userId = null;
-        return tasksMap.set(id, task);
+        return tasksMap.set(id, task as TaskType);
       });
   }
 }
 
-module.exports = TaskRepository;
+export default TaskRepository;
