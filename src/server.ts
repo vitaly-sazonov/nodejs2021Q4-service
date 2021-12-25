e1005e71f528e4e3e7953f51dcd48aa5403e67f1
vitomax <v.o.sazonov@gmail.com>
2021-12-25 23:38:33 +0300

    feat: add uncaughtException/unhandledRejection handlers and connect logger transport

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,31 @@
 import Fastify, { FastifyInstance } from 'fastify';
 
 import config from './common/config';
+import logger from './common/logger';
 import app from './app';
 
-const fastify: FastifyInstance = Fastify({});
 const { PORT } = config;
+
+const fastify: FastifyInstance = Fastify({
+  logger,
+});
 
 app(fastify, {})
   .then((server) => server.listen(Number(PORT), () => console.log(`App is running on http://localhost:${PORT}`)))
   .catch(console.error);
+
+process.on('uncaughtException', (err) => {
+  console.error('unhandledRejection event, see error.log');
+  fastify.log.fatal({ msg: 'uncaughtException event', err });
+  process.exit(1);
+});
+
+process.on('unhandledRejection', () => {
+  console.error('unhandledRejection event, see error.log');
+  fastify.log.fatal({ msg: 'unhandledRejection event' });
+  process.exit(1);
+});
+
+// Promise.reject(Error('PROM! Oops!'));
+
+throw Error('Oops!');
